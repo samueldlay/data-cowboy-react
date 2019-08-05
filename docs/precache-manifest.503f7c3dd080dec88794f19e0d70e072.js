72e38c9bc285f635fa3cd99f0e61eefd8aced45f
Samuel <22435185+samueldlay@users.noreply.github.com>
2019-08-05 13:05:26 -0500

    rename folder to docs

---
+++ docs/precache-manifest.503f7c3dd080dec88794f19e0d70e072.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7141dee76689138ee20afd7507a5c1",
+    "url": "/data-cowboy-react/index.html"
+  },
+  {
+    "revision": "44fa0999b34f0da1e474",
+    "url": "/data-cowboy-react/static/css/main.0a9f0192.chunk.css"
+  },
+  {
+    "revision": "e1fe3fb7fce84a369655",
+    "url": "/data-cowboy-react/static/js/2.273b4feb.chunk.js"
+  },
+  {
+    "revision": "44fa0999b34f0da1e474",
+    "url": "/data-cowboy-react/static/js/main.c4ed3f6b.chunk.js"
+  },
+  {
+    "revision": "3ac84f5bee7378f2fb51",
+    "url": "/data-cowboy-react/static/js/runtime~main.fe4aab2e.js"
+  }
+]);
